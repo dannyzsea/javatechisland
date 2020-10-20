61418d624746a949353cd3048dacbd59356e8138
John Doe <javatechisland@gmail.com>
2020-10-20 16:51:21 -0700

    I updated root _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 import "../styles/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from "../components/shared/Nav";
+import WebNav from "../components/shared/WebNav";
 import Banner from "../components/shared/Banner";
 
 function MyApp({ Component, pageProps }) {
+ const  isHomePage=()=>Component.name==="Home";
   return (
     <div className="portfolio-app">
-    <Nav/>
-   {Component.name==="Home"&&<Banner/>} 
+    <WebNav/>
+   {isHomePage() && <Banner/>} 
     <div className="container">
   <Component {...pageProps} />
   </div>
+  {
+    isHomePage() &&
+  <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+        <div className="container text-center">
+          <small>Copyright &copy; Your Website</small>
+        </div>
+      </footer>
+      }
 </div>
     );
 }
